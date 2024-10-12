c51ac24004b67d0a18cc10ebad3a656ba22fd328
Ерлыков Михаил <Erlykov.M.O@sbertech.ru>
2024-10-12 15:30:13 +0300

    cards

--- main.go
+++ main.go
@@ -1,14 +1,12 @@
 package main
 
-import "fmt"
-
 func main() {
-	m := map[uint16]string{
-		1: "один",
-		2: "два",
-		3: "три",
-	}
-	fmt.Println(m[1]) // "один"
-	fmt.Println(m[2]) // "два"
-	fmt.Println(m[3]) // "три"
+	cards := deck{"Ace of Diamonds, ", newCard()}
+	cards = append(cards, "Six of Spades")
+
+	cards.print()
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
