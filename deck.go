2adf6cae75b7cdad7b59a10d05d1ae946fd76c95
Ерлыков Михаил <Erlykov.M.O@sbertech.ru>
2024-10-13 22:14:29 +0300

    newCards func

--- deck.go
+++ deck.go
@@ -4,6 +4,18 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck {
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardVaules := []string{"Ace", "Two", "Three", "Four"}
+
+	for i, suit := range cardSuits {
+		for j, value := range cardVaules {
+			cards = append(cards, suit+" of "+value)
+		}
+	}
+	return cards
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
