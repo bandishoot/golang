5b1c9f5812eb581fc6b48386e8fc91863303ad88
Ерлыков Михаил <Erlykov.M.O@sbertech.ru>
2024-10-15 18:36:07 +0300

    15 10

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
-	cards := newDeck()
-
-	hand, remainingCards := deal(cards, 5)
-
-	hand.print()
-	remainingCards.print()
+	//cards := newDeck()
+	//
+	//hand, remainingCards := deal(cards, 5)
+	//
+	//hand.print()
+	//remainingCards.print()
+	greeting := "Hi there!"
+	fmt.Println([]byte(greeting))
 }
